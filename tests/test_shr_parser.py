a2ae30e5a635e7f384bb36ba00873603a9b4e7e8
Tom Schmitz <tschmitz@andrew.cmu.edu>
2025-07-23 18:16:53 -0400

    Add a test to open valid files

--- tests/test_shr_parser.py
+++ tests/test_shr_parser.py
@@ -1,5 +1,6 @@
 from shr_parser import ShrFileParser, ShrFileParserException, ShrFileParserWarning
 import pytest
+import pkg_resources
 
 
 def test_open_file_does_not_exist():
@@ -56,5 +57,14 @@ def test_incomplete_file(tmp_path):
     cut = ShrFileParser(str(f))
     with pytest.raises(ShrFileParserWarning):
         cut.open()
+    assert cut._ShrFileParser__f is not None
+    assert not cut._ShrFileParser__f.closed
+
+
+def test_open_valid_file():
+    f = pkg_resources.resource_filename(__name__, 'test_files/sweep0.shr')
+
+    cut = ShrFileParser(str(f))
+    cut.open()
     assert cut._ShrFileParser__f is not None
     assert not cut._ShrFileParser__f.closed
