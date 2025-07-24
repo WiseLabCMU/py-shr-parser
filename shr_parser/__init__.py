34a45e3c26da332317be3b2fbe046e3cb397fb83
Tom Schmitz <tschmitz@andrew.cmu.edu>
2025-07-24 14:16:05 -0400

    Add version file

--- shr_parser/__init__.py
+++ shr_parser/__init__.py
@@ -2,3 +2,4 @@ from .shr_parser import ShrSweep, ShrFileParser
 from .enumerations import ShrScale, ShrWindow, ShrDecimationDetector, ShrVideoDetector, ShrVideoUnits, ShrDecimationType, ShrChannelizerOutputUnits
 from .exceptions import ShrFileParserException, FileNotOpenError, ShrFileParserWarning
 from .metadata import ShrSweepHeader, ShrFileHeader
+from .version import __version__
